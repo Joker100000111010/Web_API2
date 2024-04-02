3296820c6350cc8af8a3171cb077235cb71924d7
Web_API2 <tranvuong261278@gmail.com>
2024-04-02 23:16:30 +0700

    init project

---
+++ Models/Students.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webAPI1.Models
+{
+    public class Students
+    {
+        [Key]
+        public Guid? StudentId { get; set; }
+        public string? Name { get; set; }
+        public List<StudentCourses>? StudentCourses { get; set; }
+    }
+}
